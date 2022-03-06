4349998ebc894c8da5995118bbfd81bf2ae53374
sh-nambu <nmci_98@yahoo.co.jp>
2022-03-06 17:07:04 +0900

    feat:ツールバー実装

--- app/src/main/java/com/meow/toilet/log/activity/MainActivity.kt
+++ app/src/main/java/com/meow/toilet/log/activity/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.meow.toilet.log.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.meow.toilet.log.R
 import com.meow.toilet.log.databinding.ActivityMainBinding
 import com.meow.toilet.log.repository.DataRepository
 import org.koin.android.ext.android.inject
@@ -27,9 +33,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        // ナビゲーションを設定する
-//        val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment).navController
-//        val appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
+        // ツールバーを設定する
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment).navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            toolbar.visibility =
+                when (destination.id) {
+                    // スプラッシュとフッターがある画面ではツールバーを表示しない
+                    R.id.splash_fragment, R.id.footer_tab_fragment -> View.GONE
+                    else -> View.VISIBLE
+                }
+        }
     }
+
+    override fun onSupportNavigateUp() =
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment).navController.navigateUp()
 }
